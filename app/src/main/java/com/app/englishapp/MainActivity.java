85006e82673180fcf42f6dd08aee9aa4126c00ea
Tomas <tparra@unal.edu.co>
2021-07-09 18:50:34 -0500

    First official commit Alpha 0.0.1

--- app/src/main/java/com/app/englishapp/MainActivity.java
+++ app/src/main/java/com/app/englishapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.app.englishapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -7,38 +8,58 @@ import android.app.SearchManager;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     InputStream inputStream = null;
-    Scanner sc = null;
-    String result = "";
-    Button btn_words, btn_search;
-    EditText edit_search;
-    TextView todayWords, resultSearch;
+    LinearLayout resultButtons;
+    Button btn_words, btn_firstWord, btn_secondWord, btn_thirdWord, btn_fourthWord, btn_search, btn_report;
+    TextView resultPhrases;
     List<String> words;
     List<String> dailyWords;
+    String urlTranslate, selectedWord;
     ProgressDialog pd;
 
     @Override
@@ -47,135 +68,326 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_words = findViewById(R.id.btn_words);
-        btn_search = findViewById(R.id.btn_search);
-        resultSearch = findViewById(R.id.result);
-        edit_search = findViewById(R.id.edit_search);
-        todayWords = findViewById(R.id.words);
+        resultButtons = findViewById(R.id.resultButtons);
+        resultPhrases = findViewById(R.id.resultPhrases);
+        btn_firstWord = findViewById(R.id.firsWord);
+        btn_secondWord = findViewById(R.id.secondWord);
+        btn_thirdWord = findViewById(R.id.thirdWord);
+        btn_fourthWord= findViewById(R.id.fourthWord);
+        btn_search = findViewById(R.id.search);
+        btn_report = findViewById(R.id.report);
+
         dailyWords = new ArrayList<>();
+        words = new ArrayList<>();
 
-        todayWords.setVisibility(View.GONE);
-
-        btn_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String words = edit_search.getText().toString();
-                if(!words.equals("")){
-                    getBodyText(words);
-                }
-            }
-        });
+        resultButtons.setVisibility(View.GONE);
+        btn_report.setVisibility(View.GONE);
+        btn_search.setVisibility(View.GONE);
+        //resultPhrases.setVisibility(View.GONE);
 
         btn_words.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                pd = ProgressDialog.show(MainActivity.this, "Creando base de datos","Espera un momento por favor", true);
-                btn_words.setEnabled(false);
-                readWords();
-                pd.setMessage("Escogiedo palabras");
-                Random random = new Random();
-                dailyWords.clear();
+                ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Wait a moment please");
+                progressDialog.show();
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        words.clear();
+                        dailyWords.clear();
 
-                for(int i = 0; i < 4; i++){
-                    String randomElement = words.get(random.nextInt(words.size()));
-                    dailyWords.add(randomElement);
+                        randomLine();
+
+                        btn_firstWord.setText(words.get(0));
+                        btn_secondWord.setText(words.get(1));
+                        btn_thirdWord.setText(words.get(2));
+                        btn_fourthWord.setText(words.get(3));
+
+                        pd = new ProgressDialog(MainActivity.this);
+
+                        getBodyText(btn_firstWord.getText().toString());
+                        getBodyText(btn_secondWord.getText().toString());
+                        getBodyText(btn_thirdWord.getText().toString());
+                        getBodyText(btn_fourthWord.getText().toString());
+
+                        resultButtons.setVisibility(View.VISIBLE);
+                        btn_words.setText("Give me another round");
+
+                        progressDialog.dismiss();
+                    }
+                }, 300);
+            }
+        });
+
+        btn_firstWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultPhrases.setText("");
+                resultPhrases.setText(Html.fromHtml(displayWords(btn_firstWord.getText().toString())));
+
+                if(resultPhrases.getText().toString().equals("")){
+                    resultPhrases.setText("Wow! It seems that this word is not yet found");
+                    btn_report.setVisibility(View.VISIBLE);
+                    selectedWord = "";
                 }
-                String total = "";
-                for(String s : dailyWords){
-                    total = total + s+"\n";
-                    todayWords.setText(total);
+                else {
+                    btn_report.setVisibility(View.GONE);
+                    btn_search.setVisibility(View.VISIBLE);
+                    selectedWord = btn_firstWord.getText().toString();
                 }
-                todayWords.setVisibility(View.VISIBLE);
-                btn_words.setEnabled(true);
+            }
+        });
+
+        btn_secondWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultPhrases.setText("");
+                resultPhrases.setText(Html.fromHtml(displayWords(btn_secondWord.getText().toString())));
+
+                if(resultPhrases.getText().toString().equals("")){
+                    resultPhrases.setText("Wow! It seems that this word is not yet found");
+                    btn_report.setVisibility(View.VISIBLE);
+                    selectedWord = "";
+                }
+                else {
+                    btn_report.setVisibility(View.GONE);
+                    btn_search.setVisibility(View.VISIBLE);
+                    selectedWord = btn_secondWord.getText().toString();
+                }
+            }
+        });
+
+        btn_thirdWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultPhrases.setText("");
+                resultPhrases.setText(Html.fromHtml(displayWords(btn_thirdWord.getText().toString())));
+
+                if(resultPhrases.getText().toString().equals("")){
+                    resultPhrases.setText("Wow! It seems that this word is not yet found");
+                    btn_report.setVisibility(View.VISIBLE);
+                    selectedWord = "";
+                }
+                else {
+                    btn_report.setVisibility(View.GONE);
+                    btn_search.setVisibility(View.VISIBLE);
+                    selectedWord = btn_thirdWord.getText().toString();
+                }
+            }
+        });
+
+        btn_fourthWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                resultPhrases.setText("");
+                resultPhrases.setText(Html.fromHtml(displayWords(btn_fourthWord.getText().toString())));
+
+                if(resultPhrases.getText().toString().equals("")){
+                    resultPhrases.setText("Wow! It seems that this word is not yet found");
+                    btn_report.setVisibility(View.VISIBLE);
+                    selectedWord = "";
+                }
+                else {
+                    btn_report.setVisibility(View.GONE);
+                    btn_search.setVisibility(View.VISIBLE);
+                    selectedWord = btn_fourthWord.getText().toString();
+                }
+            }
+        });
+
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                try{
+                    if(selectedWord != null){
+
+                        if(!selectedWord.equals("")) {
+
+                            urlTranslate = "https://translate.google.com/?hl=es&sl=en&tl=es&text=";
+                            String parametters = "";
+
+                            for (String s : dailyWords) {
+                                if (s.toLowerCase().contains(selectedWord)) {
+                                    for (String w : s.split(" ")) {
+                                        parametters = parametters + w + "%20";
+                                    }
+                                    parametters = parametters + "%0A%0A";
+                                }
+                            }
+
+                            urlTranslate = urlTranslate + parametters + "&op=translate";
+
+                            openWebURL(urlTranslate);
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "This word was not found. Sorry D:", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Sorry, we have a problem. Please try again", Toast.LENGTH_LONG).show();
+                    }
+                }catch (Exception e){
+                    Log.e("Error", e.getMessage());
+                    Toast.makeText(MainActivity.this, "Sorry, we have a problem. Please try again", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
     }
 
-    private void getBodyText(String seachedWord) {
+    private void getBodyText(String searchedWord) {
+
         new Thread(new Runnable() {
+
+            boolean isRunnung = false;
+
+            public void exit(boolean exit){
+                this.isRunnung = exit;
+            }
+
             @Override
             public void run() {
-                final StringBuilder builder = new StringBuilder();
 
-                try {
-                    String url="https://sentence.yourdictionary.com/" + seachedWord;//your website url
-                    Document doc = Jsoup.connect(url).get();
+                while(!isRunnung) {
 
-                    Element body = doc.body();
-                    builder.append(body.text());
+                    isRunnung = true;
 
-                } catch (Exception e) {
-                    builder.append("Error : ").append(e.getMessage()).append("\n");
-                }
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        String search = builder.toString().substring(493,1021);
-                        String phrases = "";
-                        List<String> wordsInPhrases = new ArrayList<>();
-                        boolean nextWord = false;
-
-                        for(String s : search.split(" ")){
-                            if(!s.contains("[1234567890]")){
-                                phrases = phrases +" "+ s;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            resultPhrases.setText("");
+                            pd.setMessage("We are working on this, please wait");
+                            if(!pd.isShowing()){
+                                pd.show();
                             }
-                            else {
-                                wordsInPhrases.add(phrases);
+                        }
+                    });
+
+                    String phrase = "";
+
+                    try {
+                        Document doc = Jsoup.parse(readURL("https://www.wordhippo.com/what-is/sentences-with-the-word/" + searchedWord + ".html"));
+
+                        if(doc.getElementById("gexv2row1").text() != null){
+
+                            String sentence1 = doc.getElementById("gexv2row1").text();
+                            dailyWords.add(sentence1);
+
+                            if(doc.getElementById("gexv2row2").text() != null){
+
+                                String sentence2 = doc.getElementById("gexv2row2").text();
+                                dailyWords.add(sentence2);
+
+                                if(doc.getElementById("gexv2row3").text() != null){
+
+                                    String sentence3 = doc.getElementById("gexv2row3").text();
+                                    dailyWords.add(sentence3);
+
+                                }
+
                             }
-                            phrases = "";
+
                         }
 
-                        for(String s : wordsInPhrases){
-                            Log.w("palabras", s);
-                        }
-                        resultSearch.setText(search);
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+
+                                pd.dismiss();
+                                isRunnung = false;
+
+                            }
+
+                        });
+
+                    } catch (Exception e) {
+                        Log.e("Error", e.getMessage());
+                        pd.dismiss();
                     }
-                });
+                }
             }
         }).start();
     }
 
-    public void searchNet(String words){
-        try{
-            Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-            intent.putExtra(SearchManager.QUERY, words);
-            startActivity(intent);
+    public void randomLine(){
+
+        for(int i = 0; i < 4; i++){
+
+            try{
+                inputStream = getAssets().open("engmix.txt");
+                String result = null;
+                Random rand = new Random();
+                int n = 0;
+                Scanner sc = new Scanner(inputStream);
+
+                while (sc.hasNext()) {
+                    ++n;
+                    String line = sc.nextLine();
+                    if (rand.nextInt(n) == 0)
+                        result = line;
+                }
+
+                sc.close();
+
+                words.add(result.toString());
+
+            }
+            catch (Exception ex){
+                Log.e("Error",ex.getMessage());
+                Toast.makeText(MainActivity.this,"Lo sentimos, por favor intenta de nuevo", Toast.LENGTH_LONG).show();
+            }
+
         }
-        catch (ActivityNotFoundException ex){
-            ex.printStackTrace();
-            searchNetCompat(words);
-        }
+
     }
 
-    private void searchNetCompat(String words){
-        try{
-            Uri uri = Uri.parse("https://sentence.yourdictionary.com/" + words);
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            startActivity(intent);
-        }
-        catch (ActivityNotFoundException ex){
-            ex.printStackTrace();
-            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-        }
-    }
+    public String readURL(String url) {
 
-    private void readWords(){
-
-        words = new ArrayList<>();
+        String fileContents = "";
+        String currentLine = "";
 
         try {
-            inputStream = getAssets().open("engmix.txt");
-            sc = new Scanner(inputStream);
-
-            while (sc.hasNext()){
-                result = sc.next();
-                words.add(result);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
+            fileContents = reader.readLine();
+            while (currentLine != null) {
+                currentLine = reader.readLine();
+                fileContents += "\n" + currentLine;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            reader.close();
+            reader = null;
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
         }
-        pd.dismiss();
 
+        return fileContents;
     }
+
+    public void openWebURL( String inURL ) {
+        Intent browse = new Intent( Intent.ACTION_VIEW , Uri.parse( inURL ) );
+
+        startActivity( browse );
+    }
+
+    public String displayWords(String word){
+
+        String finalExample = "";
+
+        for(String s : dailyWords){
+            if(s.toLowerCase().contains(word)){
+                for(String w : s.split(" ")){
+                    if(w.toLowerCase().contains(word)){
+                        w="<font color='#EE0000'>"+word+"</font>";
+                    }
+                    finalExample = finalExample + w + " ";
+                }
+                finalExample = finalExample + "<br/><br/>";
+            }
+        }
+
+        return finalExample;
+    }
+
 }
